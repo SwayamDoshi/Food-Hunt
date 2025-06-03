49346c5fb60f4d615ea58ec47ebb1b6bb5dd6692
SwayamDoshi <swayamdoshi2004@gmail.com>
2025-06-03 17:24:17 +0530

    Made models for Users Table and migrated to the mysql database

--- Food_Hunt/users/models.py
+++ Food_Hunt/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+USER_TYPES = (
+        ('admin', 'admin'),
+        ('user', 'user'),
+        ('restaurant', 'restaurant'),
+    )
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True,)
+    user_name = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=20, choices=USER_TYPES)
+    email_ID = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.username
