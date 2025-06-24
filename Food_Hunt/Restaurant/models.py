3f5295b6a1b2a7cc9f0868df5881441b6f6d277e
SwayamDoshi <swayamdoshi2004@gmail.com>
2025-06-24 16:25:12 +0530

    made order and item models and migrated

--- Food_Hunt/Restaurant/models.py
+++ Food_Hunt/Restaurant/models.py
@@ -34,3 +34,26 @@ class Menu(models.Model):
 
     def __str__(self):
         return f"{self.dishName} ({self.meal_type})"
+    
+
+class Item(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
+    item_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.item_name
+    
+
+from django.db import models
+from users.models import Users
+from .models import Restaurant, Menu  # adjust if Menu is in a different app
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='orders')
+    restaurant_id = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='orders')
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='orders')
+
+    def __str__(self):
+        return f"Order #{self.order_id} by {self.user.user_name}"
